35297eae5a1aed328edc42035efd298d47ec3043
ricardoldequi <ricardo.dequi02@gmail.com>
2023-05-31 09:44:35 -0300

    ajuste para desabilitar cors

--- dekamilk-api/src/main/java/com/dekamilk/api/model/CadastroAnimal.java
+++ dekamilk-api/src/main/java/com/dekamilk/api/model/CadastroAnimal.java
@@ -9,8 +9,6 @@ import java.sql.Date;
 
 import jakarta.persistence.Column;
 	import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 	import jakarta.persistence.GenerationType;
 	import jakarta.persistence.Id;
